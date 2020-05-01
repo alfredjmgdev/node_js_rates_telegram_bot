df2f1b0f57f35c12d2f42ccf87dc53929cfceb80
alfredjmg <alfredomujicagodoy@gmail.com>
2020-05-01 03:23:14 -0400

    heroku

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function hey () {
-    console.log("hey");
+//Importamos Express tras instalarlo vía NPM
+var express = require('express');
+var App = express();
+
+// Definimos algunas variables que usaremos en las distintas funciones
+const port = process.env.PORT || 3000;
+
+const options = {
+  root: __dirname
 }
 
-setInterval(hey, 1000);
+// Definimos funciones para luego usarlas al recibir una petición en el router
+function getHTML(req, res) {
+  res.sendFile('./index.html', options, (err) => {
+    if (err) throw err;
+    console.log('Sirviendo index.html')
+  })
+}
+
+// Definimos las rutas
+App.get('/', getHTML)
+
+// Escuchamos el puerto de Express
+App.listen(port, () => {
+  console.log('Aplicacion escuchando en el puerto: ' + port)
+})
