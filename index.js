9efb3b904159a4f96d5c431d194aa0b7aceaa4b1
alfredjmg <alfredomujicagodoy@gmail.com>
2020-05-01 12:26:49 -0400

    testing

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-//Importamos Express tras instalarlo vía NPM
-var express = require('express');
-var App = express();
+require("dotenv").config();
 
-// Definimos algunas variables que usaremos en las distintas funciones
-const port = process.env.PORT || 3000;
-
-const options = {
-  root: __dirname
-}
-
-// Definimos funciones para luego usarlas al recibir una petición en el router
-function getHTML() {
-  console.log("hola");
-}
-
-// Definimos las rutas
-App.get('/', getHTML)
-
-// Escuchamos el puerto de Express
-App.listen(port, () => {
-  console.log('Aplicacion escuchando en el puerto: ' + port)
-})
+var bot = require('./bot');
+require('./web')(bot);
